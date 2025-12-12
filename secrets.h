16a8cf62cfc9837f9f655ca3d51376c487a13dd0
PravalikaReddy16 <pravalikakota16@gmail.com>
2025-12-12 11:46:56 +0530

    Create secrets.h

---
+++ secrets.h
@@ -0,0 +1,12 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+#define WIFI_SSID     "YOUR_WIFI_SSID"
+#define WIFI_PASSWORD "YOUR_WIFI_PASSWORD"
+
+#define MQTT_SERVER   "broker.hivemq.com"
+#define MQTT_PORT     1883
+#define MQTT_USER     ""
+#define MQTT_PASS     ""
+
+#endif
